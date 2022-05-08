a3510cb31d0a53353783a05509e35dff96128a2e
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-07 22:44:23 -0300

    init

---
+++ src/main/java/br/unisantos/LivresEntregasApplication.java
@@ -0,0 +1,13 @@
+package br.unisantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivresEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivresEntregasApplication.class, args);
+	}
+
+}
