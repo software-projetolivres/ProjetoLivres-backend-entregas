6ef1a40442b4183d8ba5945c85117ce73dc9a290
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-12 15:10:27 -0300

    teste auth

--- src/main/java/br/unisantos/control/ConsumidorEntregasController.java
+++ src/main/java/br/unisantos/control/ConsumidorEntregasController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,6 +41,11 @@ public class ConsumidorEntregasController {
 	@PostMapping(value="/entregasInvalidas", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<ConsumidorEntregas> listarEntregasInvalidas(@RequestBody String dataEntrega) throws JsonMappingException, JsonProcessingException{
 		return consumidorEntregasService.listarEntregasInvalidas(DataEntrega.getDataEntrega(dataEntrega));
+	}
+	
+	@GetMapping
+	public String teste() {
+		return "Teste! Ok!";
 	}
 	
 }
