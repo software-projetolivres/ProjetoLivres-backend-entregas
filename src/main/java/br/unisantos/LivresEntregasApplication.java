6e2fd48e238b1cfb4c07ff8bdfa1c6ab36199c29
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-15 16:33:39 -0300

    ajustes

--- src/main/java/br/unisantos/LivresEntregasApplication.java
+++ src/main/java/br/unisantos/LivresEntregasApplication.java
@@ -3,8 +3,6 @@ package br.unisantos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.maps.GeoApiContext;
-
 @SpringBootApplication
 public class LivresEntregasApplication {
 
