9402a930674f66ffc8cc708e31066575b1a03567
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-18 08:58:07 -0300

    aj email confirm

--- src/main/java/br/unisantos/control/CadastroUsuarioController.java
+++ src/main/java/br/unisantos/control/CadastroUsuarioController.java
@@ -3,9 +3,9 @@ package br.unisantos.control;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +26,7 @@ public class CadastroUsuarioController {
 		return cadastroUsuarioService.cadastrar(cadastro);
 	}
 
-	@PutMapping(value = "/{token}")
+	@GetMapping(value = "/{token}")
 	public ResponseEntity<String> confirmarCadastro(@PathVariable String token) {
 		return cadastroUsuarioService.confirmarToken(token);
 	}
