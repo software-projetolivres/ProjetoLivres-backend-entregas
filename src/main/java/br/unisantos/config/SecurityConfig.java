4cde791b9cbf3b8b66969dd8e4ba9cc41634cd82
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-20 18:14:49 -0300

    tst2

--- src/main/java/br/unisantos/config/SecurityConfig.java
+++ src/main/java/br/unisantos/config/SecurityConfig.java
@@ -26,8 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		//nós permitiremos qualquer requisição que for para o endpoint passado
 		http.csrf().disable().authorizeRequests().antMatchers("api/cadastroUsuario/**").///api/**
-			permitAll().anyRequest().authenticated().and().formLogin().defaultSuccessUrl("http://localhost:5000/entregas", true);
-		//.loginPage("/login").defaultSuccessUrl("/home", true)
+			permitAll().anyRequest().authenticated().and().formLogin().loginPage("http://localhost:5000/login").defaultSuccessUrl("http://localhost:5000/entregas", true);
 	}
 	
 	@Override
