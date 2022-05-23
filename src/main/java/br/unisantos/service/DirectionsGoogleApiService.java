559ef1faf115351b0fb481019edd256d5dff9da8
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-22 21:20:44 -0300

    retirando os debugs

--- src/main/java/br/unisantos/service/DirectionsGoogleApiService.java
+++ src/main/java/br/unisantos/service/DirectionsGoogleApiService.java
@@ -36,20 +36,15 @@ public class DirectionsGoogleApiService {
 		System.out.println("DEBUGGGGGGGGGGGGGGGGGGG => ");
 		
 		for(int i = 0; i < directionsResult.routes[0].legs.length; i++) {
-			System.out.println("\ni: " + i + "startAddress: " + directionsResult.routes[0].legs[i].startAddress);
 			if(i > 0 && i%3 == 0) {
-				System.out.println("\n GAAAAAAAAAAABI 1");
 				if(i == (directionsResult.routes[0].legs.length - 1)) {	//se estiver na última iteração
-					System.out.println("\n GAAAAAAAAAAABI 2");
 					waypoints.add(enderecoLivres);
 					api.setDestination(directionsResult.routes[0].legs[i].startAddress);
 				} else {
-					System.out.println("\n GAAAAAAAAAAABI 3");
 					waypoints.add(directionsResult.routes[0].legs[i].startAddress);
 					waypoints.add(enderecoLivres);
 				}
 			} else if(i > 0) {
-				System.out.println("\n GAAAAAAAAAAABI 4");
 				waypoints.add(directionsResult.routes[0].legs[i].startAddress);
 			}
 		}
