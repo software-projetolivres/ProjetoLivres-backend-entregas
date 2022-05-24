c5446ba2d2836a9fc52d20286d287ce7ab65dd43
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 18:11:40 -0300

    ajuste allow cors

--- src/main/java/br/unisantos/config/CorsConfiguration.java
+++ src/main/java/br/unisantos/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5000")
+            .allowedOrigins(System.getenv("link_site"))
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
