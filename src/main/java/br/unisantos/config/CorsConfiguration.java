937f130895cf316be4febac343aa97560b34dc74
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 18:15:40 -0300

    ajuste allow cors

--- src/main/java/br/unisantos/config/CorsConfiguration.java
+++ src/main/java/br/unisantos/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(System.getenv("link_site"))
+            .allowedOrigins("https://livres-entregas-fe.herokuapp.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
