05264bdf1a160169e3377dae337795e8ac734d01
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-25 00:03:56 -0300

    aj cors dinâmico

--- src/main/java/br/unisantos/config/CorsConfiguration.java
+++ src/main/java/br/unisantos/config/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package br.unisantos.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +10,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+    	String origensPermitidas = StringUtils.chop(System.getenv("link_site"));
+    	
         registry.addMapping("/**")
-            .allowedOrigins("https://livres-entregas-fe.herokuapp.com")
+            .allowedOrigins(origensPermitidas)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
