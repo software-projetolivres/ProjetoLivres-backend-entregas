42d0d1dd5375e8492bd172b4960b7678c019f099
gabsperandeo <62123194+gabsperandeo@users.noreply.github.com>
2022-07-24 14:54:54 -0300

    Update SecurityConfig.java

--- src/main/java/br/unisantos/config/SecurityConfig.java
+++ src/main/java/br/unisantos/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package br.unisantos.config;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.unisantos.service.UsuarioService;
 
